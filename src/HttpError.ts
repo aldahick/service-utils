1c611235d3a8721a4adb712a88405666c92fbad9
Alex Hicks <alex@alexhicks.net>
2019-12-03 19:40:35 -0500

    Add more HttpErrors (and npm-script bump)

--- src/HttpError.ts
+++ src/HttpError.ts
@@ -4,7 +4,9 @@ export class HttpError extends Error {
     public error: string | Error
   ) { super(typeof(error) === "string" ? error : undefined); }
 
-  static unauthorized = (message = "Unauthorized") => new HttpError(403, message);
+  static badRequest = (message = "Bad request (are you providing the correct parameters?)") => new HttpError(400, message);
+  static unauthorized = (message = "Unauthorized (are you providing credentials?)") => new HttpError(401, message);
+  static forbidden = (message = "Forbidden (do you have the right permissions?)") => new HttpError(403, message);
   static notFound = (message = "Not found") => new HttpError(404, message);
   static internalError = (message = "Internal server error") => new HttpError(500, message);
 }
