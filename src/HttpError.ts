5360d5788fa966abb2193f954e7bdf33e74640ba
Alex Hicks <alex@alexhicks.net>
2019-12-03 19:20:48 -0500

    Refactor HttpErrors

--- src/HttpError.ts
+++ src/HttpError.ts
@@ -3,8 +3,8 @@ export class HttpError extends Error {
     public status: number,
     public error: string | Error
   ) { super(typeof(error) === "string" ? error : undefined); }
-}
 
-export const UnauthorizedError = (message = "Unauthorized") => new HttpError(403, message);
-export const NotFoundError = (message = "Not found") => new HttpError(404, message);
-export const InternalServerError = (message = "Internal server error") => new HttpError(500, message);
+  static unauthorized = (message = "Unauthorized") => new HttpError(403, message);
+  static notFound = (message = "Not found") => new HttpError(404, message);
+  static internalError = (message = "Internal server error") => new HttpError(500, message);
+}
